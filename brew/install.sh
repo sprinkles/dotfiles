d2bd891439d3f2924ab2400ee5921094f950544a
sprinkles <elizabeth.m.stanley@gmail.com>
2015-03-31 12:38:45 -0500

    Switching to brew brewdle, instead of using the older brew bundle setup

--- brew/install.sh
+++ brew/install.sh
@@ -5,6 +5,7 @@ if ! which brew > /dev/null ; then
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 fi
 
-# Install kegs and casks
-brew bundle Brewfile
-brew bundle Caskfile
+brew update && brew upgrade
+brew tap homebrew/brewdler
+brew brewdle
+brew cleanup
